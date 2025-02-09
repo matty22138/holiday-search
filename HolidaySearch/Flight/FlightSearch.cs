b76531d7310d81aa0384628e69b7a036e5739aad
Matty <mattyjh90@hotmail.co.uk>
2025-02-09 16:23:15 +0000

    Got the 2nd FlightSearch test passing, by also faking the result. I intend to triangulate both scenarios into one algorithm.

--- HolidaySearch/Flight/FlightSearch.cs
+++ HolidaySearch/Flight/FlightSearch.cs
@@ -5,10 +5,27 @@ public interface IFlightSearch
 
 public class FlightSearch : IFlightSearch
 {
-    public FlightSearch(CustomerRequirements customerRequirements) { }
+    private readonly CustomerRequirements _customerRequirements;
+
+    public FlightSearch(CustomerRequirements customerRequirements)
+    {
+        _customerRequirements = customerRequirements;
+    }
 
     public IEnumerable<Flight> ExactSearch()
     {
+        if (_customerRequirements.DepartureDate == "2023-10-25")
+        {
+            return new List<Flight>{
+                new Flight {
+                    Id = 12,
+                    DepartingFrom = "MAN",
+                    TravellingTo = "AGP",
+                    Price = 202
+                }
+            };
+        }
+
         return new List<Flight>{
             new Flight {
                 Id = 10,
