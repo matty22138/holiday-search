48780acb28b5a1c650079201208f53494d2cf117
Matty <mattyjh90@hotmail.co.uk>
2025-02-09 17:18:16 +0000

    Un-commented and corrected feature test, and ensured it was passing, which confirms the extact-search feature has been implemented outside-in

--- HolidaySearch.Tests/HolidaySearchFeatureTests.cs
+++ HolidaySearch.Tests/HolidaySearchFeatureTests.cs
@@ -10,23 +10,27 @@ public class HolidaySearchFeatureTests
     [Test]
     public void ExactSearchForAHoliday_WithCustomerRequirments_ReturnsTheBestOfTheMatchingResults()
     {
-        // var holidaySearch = new HolidaySearch(new FlightSearch(new CustomerRequirements
-        // {
-        //     DepartingFrom = "MAN",
-        //     TravelingTo = "AGP",
-        //     DepartureDate = "2023/07/01",
-        //     Duration = 7
-        // }), new HotelSearch());
+        var customerRequirements = new CustomerRequirements
+        {
+            DepartingFrom = "MAN",
+            TravelingTo = "AGP",
+            DepartureDate = "2023-07-01",
+            Duration = 7
+        };
+        var holidaySearch = new HolidaySearch(
+            new FlightSearch(customerRequirements),
+            new HotelSearch(customerRequirements));
 
-        // var result = holidaySearch.ExactSearch().First();
+        var result = holidaySearch.ExactSearch().First();
 
-        // Assert.That(result.TotalPrice, Is.EqualTo("328.00"));
-        // Assert.That(result.Flight.Id, Is.EqualTo(2));
-        // Assert.That(result.Flight.DepartingFrom, Is.EqualTo("MAN"));
-        // Assert.That(result.Flight.TravellingTo, Is.EqualTo("AGP"));
-        // Assert.That(result.Flight.Price, Is.EqualTo("£245.00"));
-        // Assert.That(result.Hotel.Id, Is.EqualTo(9));
-        // Assert.That(result.Hotel.Name, Is.EqualTo("Nh Malaga"));
-        // Assert.That(result.Hotel.TotalPrice, Is.EqualTo(83.00));
+        Assert.That(result.TotalPrice, Is.EqualTo(826));
+        Assert.That(result.Flight.Id, Is.EqualTo(2));
+        Assert.That(result.Flight.DepartingFrom, Is.EqualTo("MAN"));
+        Assert.That(result.Flight.TravellingTo, Is.EqualTo("AGP"));
+        Assert.That(result.Flight.Price, Is.EqualTo(245));
+        Assert.That(result.Hotel.Id, Is.EqualTo(9));
+        Assert.That(result.Hotel.Name, Is.EqualTo("Nh Malaga"));
+        Assert.That(result.Hotel.PricePerNight, Is.EqualTo(83));
+        Assert.That(result.Hotel.TotalPrice, Is.EqualTo(581));
     }
 }
