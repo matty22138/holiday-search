c01e25393323da89266e05da12b33ced9fb6dcd1
Matty <mattyjh90@hotmail.co.uk>
2025-02-09 14:02:31 +0000

    Added another failing test, this time I'm testing the flight matching with different requirements

--- HolidaySearch.Tests/HolidaySearchUnitTests.cs
+++ HolidaySearch.Tests/HolidaySearchUnitTests.cs
@@ -25,4 +25,23 @@ public class HolidaySearchUnitTests
         Assert.That(result.Flight.TravellingTo, Is.EqualTo("TFS"));
         Assert.That(result.Flight.Price, Is.EqualTo("£470.00"));
     }
+
+    [Test]
+    public void GetResults_WithDifferentCustomerRequirments_ReturnsTheBestFlightInTheResults()
+    {
+        var holidaySearch = new HolidaySearch(new CustomerRequirements
+        {
+            DepartingFrom = "MAN",
+            TravelingTo = "LPA",
+            DepartureDate = "2022-11-10",
+            Duration = 7
+        });
+
+        var result = holidaySearch.GetResults().First();
+
+        Assert.That(result.Flight.Id, Is.EqualTo(1));
+        Assert.That(result.Flight.DepartingFrom, Is.EqualTo("MAN"));
+        Assert.That(result.Flight.TravellingTo, Is.EqualTo("LPA"));
+        Assert.That(result.Flight.Price, Is.EqualTo("£125.00"));
+    }
 }
