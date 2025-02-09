ab03df4824a169d5924e996f1edaca3ba65fe188
Matty <mattyjh90@hotmail.co.uk>
2025-02-09 16:20:29 +0000

    Created another failing test, to test the Flight search functionality with different requirements

--- HolidaySearch.Tests/FlightSearchUnitTests.cs
+++ HolidaySearch.Tests/FlightSearchUnitTests.cs
@@ -28,4 +28,23 @@ public class FlightSearchUnitTests
         Assert.That(result.TravellingTo, Is.EqualTo("AGP"));
         Assert.That(result.Price, Is.EqualTo(225));
     }
+
+    [Test]
+    public void ExactSearch_WithDifferentCustomerRequirments_ReturnsTheExactMatchingFlight()
+    {
+        var holidaySearch = new FlightSearch(new CustomerRequirements()
+        {
+            DepartingFrom = "MAN",
+            TravelingTo = "AGP",
+            DepartureDate = "2023-10-25",
+            Duration = 7
+        });
+
+        var result = holidaySearch.ExactSearch().First();
+
+        Assert.That(result.Id, Is.EqualTo(12));
+        Assert.That(result.DepartingFrom, Is.EqualTo("MAN"));
+        Assert.That(result.TravellingTo, Is.EqualTo("AGP"));
+        Assert.That(result.Price, Is.EqualTo(202));
+    }
 }
